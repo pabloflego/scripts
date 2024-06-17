f66b3d10c5bf6784de605b50203363307d7bc58b
Pablo Flego <pablinff@gmail.com>
2024-06-17 22:18:19 +0200

    fix issue with wrong dns server on wsl2

---
+++ wsl2-set-dns.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Usage:
+# bash -c "$(wget -qLO - https://github.com/pabloflego/scripts/raw/main/wsl2-set-dns.sh)"
+
+sudo rm /etc/resolve.conf
+sudo echo "nameserver 10.1.1.1" > /etc/resolve.conf
+sudo chattr +i /etc/resolv.conf
