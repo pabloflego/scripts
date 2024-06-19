7144af821aebe701afc1ccdb6ed352452e8a83bd
Pablo Flego <pablinff@gmail.com>
2024-06-19 21:12:10 +0200

    check-enable-ssh: give more feedback

--- check-enable-ssh.sh
+++ check-enable-ssh.sh
@@ -11,16 +11,26 @@ NC=$(tput sgr0) # No Color
 
 # Check if SSH service is enabled
 if systemctl is-enabled ssh >/dev/null 2>&1; then
-    echo "${GREEN}SSH service is enabled.${NC}"
+    echo "${GREEN}SSH service is already enabled.${NC}"
 else
     echo "${RED}SSH service is not enabled. Enabling it...${NC}"
     sudo systemctl enable ssh
+    if systemctl is-enabled ssh >/dev/null 2>&1; then
+        echo "${GREEN}SSH service has been enabled.${NC}"
+    else
+        echo "${RED}Failed to enable SSH service.${NC}"
+    fi
 fi
 
 # Check if SSH service is running
 if systemctl is-active ssh >/dev/null 2>&1; then
-    echo "${GREEN}SSH service is running.${NC}"
+    echo "${GREEN}SSH service is already running.${NC}"
 else
     echo "${RED}SSH service is not running. Starting it...${NC}"
     sudo systemctl start ssh
+    if systemctl is-active ssh >/dev/null 2>&1; then
+        echo "${GREEN}SSH service has been started.${NC}"
+    else
+        echo "${RED}Failed to start SSH service.${NC}"
+    fi
 fi
